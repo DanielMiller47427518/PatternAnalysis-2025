a0edec8783abba77968aec191b758676537424ec
unknown <s4742751@uq.edu.au>
2025-10-29 14:48:56 +1000

    Implemented dataset.py including loading image data in .nii format, and converting into a DataLoader object

---
+++ recognition/dataset.py
@@ -0,0 +1,53 @@
+import numpy as np
+import torch
+import os
+from torch.utils.data import Dataset, DataLoader
+from utils import load_data_2D, BATCH_SIZE
+
+class ImageDataset(Dataset):
+    """
+    Stores images and their corresponding segmentation masks and applies transformations specified
+    Overrides __len__ and __getitem__ to allow for use with torch.utils.DataLoader
+    """
+    def __init__(self, image_dir, segmask_dir, transforms, early_stop=False):
+        image_names = [i for i in os.listdir(image_dir)]
+        mask_names = [m for m in os.listdir(segmask_dir)]
+
+        self.transforms = transforms
+
+        # load nii files using utils helper
+        # don't need to specify dimensions unless not using HIPmri
+        self.images = load_data_2D(image_names, normImage=True, early_stop=early_stop)
+        self.seg_masks = load_data_2D(mask_names, normImage=False, early_stop=early_stop)   
+
+    def __len__(self):
+        return len(self.images)
+    
+    def __getitem__(self, idx):
+        # load images into torch tensors before returning
+        img = self.images[idx]
+        msk = self.seg_masks[idx]
+
+        # convert to tensors
+        img = torch.tensor(img)
+        msk = torch.tensor(msk)
+
+        # add channel dimension, conv in torch expects 4d input including
+        # 1 channel as images are greyscale 
+        img = img.unsqueeze(0)
+        msk = msk.unsqueeze(0)
+
+        if self.transforms:
+            # using transforms vs
+            img, msk = self.transforms(img, msk)
+
+        return (img, msk)
+    
+def get_loader(image_dir, mask_dir, transforms, early_stop):
+    """
+    Takes directory of image slices and corresponding segmentation masks
+    and returns a DataLoader, to be used for training and running inference on model
+    """
+    dataset = ImageDataset(image_dir, mask_dir, transforms=transforms, early_stop=early_stop)
+    loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=6)
+    return loader
---
+++ recognition/utils.py
@@ -0,0 +1,70 @@
+import nibabel as nib
+import numpy as np
+from tqdm import tqdm
+from utils import IMAGE_HEIGHT, IMAGE_WIDTH
+from skimage.transform import resize
+BATCH_SIZE = 16
+
+
+
+def to_channels(arr: np.ndarray , dtype = np.uint8) -> np.ndarray:
+    channels = np.unique(arr)
+    res = np.zeros(arr.shape + (len(channels),), dtype=dtype)
+    for c in channels :
+        c = int(c)
+        res [..., c:c+1][arr == c] = 1
+    return res
+
+# load medical image functions
+def load_data_2D(imageNames, normImage=False, dtype=np.float32, getAffines=False, early_stop=False, image_shape=(IMAGE_HEIGHT, IMAGE_WIDTH)):
+    '''
+    Load medical image data from names , cases list provided into a list for each .
+
+    This function pre - allocates 4D arrays for conv2d to avoid excessive memory &
+    usage .
+
+    normImage : bool ( normalise the image 0.0 -1.0)
+    early_stop : Stop loading pre - maturely , leaves arrays mostly empty , for quick &
+    loading and testing scripts .
+    '''
+    affines = []
+
+    # # get fixed size
+    num = len(imageNames)
+    # first_case = nib.load(imageNames[0]).get_fdata(caching='unchanged')
+    # if len(first_case.shape) == 3:
+    #     first_case = first_case [:,:,0] # sometimes extra dims , remove
+
+
+    # rows, cols = first_case.shape
+    # set size of images based on provided target shape
+    images = np.zeros((num, image_shape[0], image_shape[1]) , dtype=dtype)
+
+    for i, inName in enumerate(tqdm(imageNames)):
+        niftiImage = nib.load(inName)
+        inImage = niftiImage.get_fdata(caching = 'unchanged') # read disk only
+        affine = niftiImage.affine
+        
+        if len(inImage.shape) == 3:
+            inImage = inImage [:,:,0] # sometimes extra dims in HipMRI_study data
+
+        inImage = inImage.astype(dtype)
+
+        # not all images are correct dimensions, resize
+        inImage = resize(inImage, (image_shape[0], image_shape[1]), order=1, preserve_range=True))
+
+        if normImage :
+            #~ inImage = inImage / np. linalg . norm ( inImage )
+            #~ inImage = 255. * inImage / inImage . max ()
+            inImage = (inImage - inImage.mean()) / inImage.std()
+        
+        images [i,:,:] = inImage
+
+        affines . append ( affine )
+        if i > 20 and early_stop :
+            break
+
+    if getAffines :
+        return images , affines
+    else :
+        return images
